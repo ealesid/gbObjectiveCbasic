3e6804384a5e2c1795b67b5e154275219fef5ea5
ealesid <ealesid@gmail.com>
2019-07-06 19:16:36 +0300

    Initial Commit

---
+++ gbObjectiveCbasic/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  gbObjectiveCbasic
+//
+//  Created by Aleksey Sidorov on 06.07.2019.
+//  Copyright © 2019 Aleksey Sidorov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
