6e625e0bd61ea17a10f05e407c0fd6a58b188fa4
ealesid <ealesid@gmail.com>
2019-07-06 19:18:38 +0300

    lesson-1

--- gbObjectiveCbasic/main.m
+++ gbObjectiveCbasic/main.m
@@ -1,17 +1,44 @@
-//
-//  main.m
-//  gbObjectiveCbasic
-//
-//  Created by Aleksey Sidorov on 06.07.2019.
-//  Copyright © 2019 Aleksey Sidorov. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        //        1st and 2nd Tasks
+        
+        printf("\n\t 1st & 2nd Tasks \n");
+        
+        double firstNumber = 0.0;
+        double secondNumber = 0.0;
+        
+        printf("1st number?: ");
+        scanf("%lf", &firstNumber);
+        printf("2nd number?: ");
+        scanf("%lf", &secondNumber);
+        
+        NSLog(@"Summ of %.2lf and %.2lf is %.2lf", firstNumber, secondNumber, firstNumber + secondNumber);
+        NSLog(@"Diff of %.2lf and %.2lf is %.2lf", firstNumber, secondNumber, firstNumber - secondNumber);
+        NSLog(@"Mulltiply of %.2lf and %.2lf is %.2lf", firstNumber, secondNumber, firstNumber * secondNumber);
+        NSLog(@"Division of %.2lf and %.2lf is %.2lf", firstNumber, secondNumber, firstNumber / secondNumber);
+        
+        //        3rd Task
+        
+        printf("\n\t 3rd Task \n");
+        
+        double firstNum;
+        double secondNum;
+        double thirdNum;
+        
+        printf("1st number?: ");
+        scanf("%lf", &firstNum);
+        printf("2nd number?: ");
+        scanf("%lf", &secondNum);
+        printf("3rd number?: ");
+        scanf("%lf", &thirdNum);
+        
+        NSLog(
+              @"Average of %.2lf, %.2lf, %.2lf is %.2lf",
+              firstNum, secondNum, thirdNum, (firstNum + secondNum + thirdNum) / 3
+              );
     }
     return 0;
 }
