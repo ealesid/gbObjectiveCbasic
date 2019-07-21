e0cd4032eba74f69181f8a7e2b7beae461f0381a
ealesid <ealesid@gmail.com>
2019-07-21 13:09:18 +0300

    1st task

--- gbObjectiveCbasic/main.m
+++ gbObjectiveCbasic/main.m
@@ -3,42 +3,19 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        //        1st and 2nd Tasks
+        //        1st Task
         
-        printf("\n\t 1st & 2nd Tasks \n");
+        NSArray *stringsArray = @[
+            @"1stString",
+            @"2ndString",
+            @"3rdString",
+            @"lastString",
+        ];
         
-        double firstNumber = 0.0;
-        double secondNumber = 0.0;
+        for (NSString *string in stringsArray) {
+            NSLog(string);
+        }
         
-        printf("1st number?: ");
-        scanf("%lf", &firstNumber);
-        printf("2nd number?: ");
-        scanf("%lf", &secondNumber);
-        
-        NSLog(@"Summ of %.2lf and %.2lf is %.2lf", firstNumber, secondNumber, firstNumber + secondNumber);
-        NSLog(@"Diff of %.2lf and %.2lf is %.2lf", firstNumber, secondNumber, firstNumber - secondNumber);
-        NSLog(@"Mulltiply of %.2lf and %.2lf is %.2lf", firstNumber, secondNumber, firstNumber * secondNumber);
-        NSLog(@"Division of %.2lf and %.2lf is %.2lf", firstNumber, secondNumber, firstNumber / secondNumber);
-        
-        //        3rd Task
-        
-        printf("\n\t 3rd Task \n");
-        
-        double firstNum;
-        double secondNum;
-        double thirdNum;
-        
-        printf("1st number?: ");
-        scanf("%lf", &firstNum);
-        printf("2nd number?: ");
-        scanf("%lf", &secondNum);
-        printf("3rd number?: ");
-        scanf("%lf", &thirdNum);
-        
-        NSLog(
-              @"Average of %.2lf, %.2lf, %.2lf is %.2lf",
-              firstNum, secondNum, thirdNum, (firstNum + secondNum + thirdNum) / 3
-              );
     }
     return 0;
 }
