3666b5c152f386206828d245ac5d63e97eacb4e2
ealesid <ealesid@gmail.com>
2019-07-22 18:15:38 +0300

    2nd task

--- gbObjectiveCbasic/main.m
+++ gbObjectiveCbasic/main.m
@@ -1,6 +1,46 @@
 #import <Foundation/Foundation.h>
 
 
+// for 2nd task
+int add(int firstNumber, int secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+int substruct(int firstNumber, int secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+int multiply(int firstNumber, int secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+int divide(int firstNumber, int secondNumber) {
+    return firstNumber / secondNumber;
+}
+
+
+int calculate(NSString *operation, int firstNumber, int secondNumber) {
+    NSArray *operations = @[@"+", @"-", @"*", @"/"];
+    int operationIndex = [operations indexOfObject: operation];
+    
+    switch (operationIndex) {
+        case 0:
+            return add(firstNumber, secondNumber);
+        case 1:
+            return substruct(firstNumber, secondNumber);
+        case 2:
+            return multiply(firstNumber, secondNumber);
+        case 3:
+            return divide(firstNumber, secondNumber);
+            
+        default:
+            NSLog(@"Operation not allowed!");
+            return 0;
+    }
+}
+
+
+// for 3rd task
 typedef enum Gender {
     Male,
     Female,
@@ -36,9 +76,20 @@ int main(int argc, const char * argv[]) {
             NSLog(@"%@", string);
         }
         
+        //      2nd Task
+        printf("\n\t** 2nd Task **\n");
+        
+        int a  = calculate(@"+", 2, 3);
+        int b  = calculate(@"-", 3, 2);
+        int c  = calculate(@"*", 4, 5);
+        int d  = calculate(@"/", 10, 2);
+        
+        NSLog(@"Result: \n a = %i, \n b = %i, \n c = %i, \n d = %i", a, b, c, d);
+        
+        
         //      3rd Task
         printf("\n\t** 3rd Task **\n");
-
+        
         typedef struct Human {
             NSString *name;
             NSInteger age;
